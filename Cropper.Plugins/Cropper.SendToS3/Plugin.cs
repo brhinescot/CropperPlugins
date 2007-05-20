b76ce3adac34b3a3146e6d1c941f83c0f6a12ac2
paltman_cp <SND\paltman_cp>
2007-05-20 20:00:39 +0000

    Added Options configuration to add Key Id and Secret Key and select a Bucket.  Not functionally complete yet.  Still need to write up the PutObject or more than likely the PutObjectInline call.

---
+++ Cropper.Plugins/Cropper.SendToS3/Plugin.cs
@@ -0,0 +1,140 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+
+using Fusion8.Cropper.Extensibility;
+using Cropper.SendToS3.com.amazonaws.s3;
+
+namespace Cropper.SendToS3
+{
+    public class Plugin : IPersistableImageFormat, IConfigurablePlugin
+    {
+        S3Settings _settings;
+        IPersistableOutput _output;
+
+        #region IPersistableImageFormat Members
+                
+        public string Description
+        {
+            get { return "Send screenshot to Amazon S3 account."; }
+        }
+
+        public string Extension
+        {
+            get { return "Png"; }
+        }
+
+        public IPersistableImageFormat Format
+        {
+            get { return this; }
+        }
+
+
+        public void Connect(IPersistableOutput persistableOutput)
+        {
+            if (persistableOutput == null)
+            {
+                throw new ArgumentNullException("persistableOutput");
+            }
+            _output = persistableOutput;
+            _output.ImageCaptured += new ImageCapturedEventHandler(this.persistableOutput_ImageCaptured);
+            _output.ImageCapturing += new ImageCapturingEventHandler(this.persistableOutput_ImageCapturing);
+        }
+
+        public void Disconnect()
+        {
+            _output.ImageCaptured -= new ImageCapturedEventHandler(this.persistableOutput_ImageCaptured);
+            _output.ImageCapturing -= new ImageCapturingEventHandler(this.persistableOutput_ImageCapturing);
+        }
+
+        public event ImageFormatClickEventHandler ImageFormatClick;
+
+        private void OnImageFormatClick(object sender, ImageFormatEventArgs e)
+        {
+            if (ImageFormatClick != null)
+            {
+                ImageFormatClick(sender, e);
+            }
+        }
+
+        void SaveImage(Stream stream, Image image)
+        {
+            AWSAuthConnection con = new AWSAuthConnection(_settings.AccessKeyId, _settings.SecretAccessKey);
+            
+            //image.Save(stream, ImageFormat.Png);
+        }
+
+        void persistableOutput_ImageCaptured(object sender, ImageCapturedEventArgs e)
+        {
+            //_output.FetchOutputStream(new StreamHandler(this.SendImage), e.ImageNames.FullSize, e.FullSizeImage);
+            //FileInfo file = new FileInfo(e.ImageNames.FullSize);
+            //string subject = replaceTokens(PluginSettings.Subject, file);
+            //string body = replaceTokens(PluginSettings.Message, file);
+            //MapiMailMessage message = new MapiMailMessage(subject, body);
+            //message.Files.Add(e.ImageNames.FullSize);
+            //message.ShowDialog();
+        }
+
+
+        void persistableOutput_ImageCapturing(object sender, ImageCapturingEventArgs e)
+        {
+        }
+
+        public MenuItem Menu
+        {
+            get
+            {
+                MenuItem item = new MenuItem();
+                item.RadioCheck = true;
+                item.Text = "Send to Amazon S3";
+                item.Click += new EventHandler(this.MenuItemClick);
+                return item;
+            }
+        }
+
+        private void MenuItemClick(object sender, EventArgs e)
+        {
+            ImageFormatEventArgs args = new ImageFormatEventArgs();
+            args.ClickedMenuItem = (MenuItem)sender;
+            args.ImageOutputFormat = this;
+            this.OnImageFormatClick(sender, args);
+        }
+
+
+        #endregion
+
+        #region IConfigurablePlugin Members
+
+        public BaseConfigurationForm ConfigurationForm
+        {
+            get { return new Options(); }
+        }
+
+        public bool HostInOptions
+        {
+            get { return true; }
+        }
+
+        public object Settings
+        {
+            get
+            {
+                if (_settings == null)
+                {
+                    _settings = new S3Settings();
+                }
+
+                return _settings;
+            }
+            set
+            {
+                _settings = value as S3Settings;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Cropper.Plugins/Cropper.SendToS3/S3Settings.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cropper.SendToS3
+{
+    public class S3Settings
+    {
+        public string AccessKeyId;
+        public string SecretAccessKey;
+        public string BucketName;
+    }
+}
