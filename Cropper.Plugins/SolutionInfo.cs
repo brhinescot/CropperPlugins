784bd95b06a5ac33085a4013ea97525677e8b674
jongalloway_cp <SND\jongalloway_cp>
2007-05-22 07:44:19 +0000

    Added to allow versioning the plugin pack

---
+++ Cropper.Plugins/SolutionInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cropper.Plugins")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
