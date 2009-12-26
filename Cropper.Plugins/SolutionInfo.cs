de79f49b3b626bc210b1120f56c37ffaea1a73a1
Cheeso_cp <SND\Cheeso_cp>
2009-12-26 18:26:27 +0000

    Updated sendTo TFS project - now uses VS2008 SDK.  Also updated solutionInfo.cs for the solution, to v1.2.

--- Cropper.Plugins/SolutionInfo.cs
+++ Cropper.Plugins/SolutionInfo.cs
@@ -1,10 +1,10 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyCompany("http://codeplex.com/cropperplugins")]
+[assembly: AssemblyCompany("http://cropperplugins.codeplex.com/")]
 [assembly: AssemblyProduct("Cropper.Plugins")]
 [assembly: AssemblyCopyright("")]
-[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("1.2.*")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
