c8f7eea5c9343d407129b218eed953860d876645
Cheeso_cp <SND\Cheeso_cp>
2010-05-11 17:01:40 +0000

    adjust reference paths to get  Cropper.TFSWorkItem.dll to build properly in Release configuration.  This is for v1.2.2.

--- Cropper.Plugins/Cropper.TFSWorkItem/Constants.cs
+++ Cropper.Plugins/Cropper.TFSWorkItem/Constants.cs
@@ -6,17 +6,16 @@ namespace Cropper.TFSWorkItem
 {
     public class Constants
     {
-        public const string PluginDescription = "TFS Work Item";
-        public const string DoNotShowOptionsDialogAgainConfigKey = "DoNotShowOptionsDialogAgain";
+        public const string PluginDescription                           = "TFS Work Item";
+        public const string DoNotShowOptionsDialogAgainConfigKey        = "DoNotShowOptionsDialogAgain";
         public const string DoNotShowCaptureOptionsDialogAgainConfigKey = "DoNotShowCaptureOptionsDialogAgain";
-        public const string TeamServerConfigKey = "TeamServer";
-        public const string TeamProjectConfigKey = "TeamProject";
-        public const string WorkItemTypeConfigKey = "WorkItemType";
-        public const string DefaultImageNameConfigKey = "DefaultImageName";
-        public const string DefaultOutputExtensionConfigKey = "DefaultOutputExtension";
-        public const string DefaultAttachmentCommentConfigKey = "DefaultAttachmentComment";
-        public const string OpenImageInEditorConfigKey = "OpenImageInEditor";
-        public const string ImageEditorConfigKey = "ImageEditor";
-        
+        public const string TeamServerConfigKey                         = "TeamServer";
+        public const string TeamProjectConfigKey                        = "TeamProject";
+        public const string WorkItemTypeConfigKey                       = "WorkItemType";
+        public const string DefaultImageNameConfigKey                   = "DefaultImageName";
+        public const string DefaultOutputExtensionConfigKey             = "DefaultOutputExtension";
+        public const string DefaultAttachmentCommentConfigKey           = "DefaultAttachmentComment";
+        public const string OpenImageInEditorConfigKey                  = "OpenImageInEditor";
+        public const string ImageEditorConfigKey                        = "ImageEditor";
     }
 }
