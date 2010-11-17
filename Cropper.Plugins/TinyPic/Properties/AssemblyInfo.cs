74d9fd596597e3400e587abe66b413d08886e27b
Cheeso_cp <SND\Cheeso_cp>
2010-11-17 12:13:09 +0000

    first update of TinyPic project - convert to TinyPic name from Cropper.SendToTinyPic

--- Cropper.Plugins/TinyPic/Properties/AssemblyInfo.cs
+++ Cropper.Plugins/TinyPic/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Cropper.SendToTinyPic")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("plugin to send pics to TinyPic.com.  (c) 2010 Dino Chiesa")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
