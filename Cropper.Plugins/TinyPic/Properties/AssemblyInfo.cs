c4cdba603c43f6c948fb2737096454f1da2d3d49
Cheeso_cp <SND\Cheeso_cp>
2010-12-05 10:35:49 +0000

    Remove TinyPic plugin.  The HTTP transaction flow has changed significantly, and a new Captcha has been inserted into the flow, so that the existing plugin doesn't work.  I could modify the plugin to work with the updated flow, but I noticed that the TinyPic terms of service say that the upload service should not be scripted.  Clearly they don't want this kind of client.  So I'm dropping it.  Instead of TinyPic, use the Imgur plugin, which works well.

--- Cropper.Plugins/TinyPic/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("Cropper.SendToTinyPic")]
-[assembly: AssemblyDescription("plugin to send pics to TinyPic.com.  (c) 2010 Dino Chiesa")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
