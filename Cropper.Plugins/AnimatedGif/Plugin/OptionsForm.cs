994f8beaac476ef8ff276ae30dec395ebdb4ffa2
Cheeso_cp <SND\Cheeso_cp>
2010-12-23 01:42:04 +0000

    workitem 14909: AnimatedGif now has an "edit after capture" option.  Also, tracing is available in AnimatedGif plugin for the first time.

--- Cropper.Plugins/AnimatedGif/Plugin/OptionsForm.cs
+++ Cropper.Plugins/AnimatedGif/Plugin/OptionsForm.cs
@@ -11,9 +11,8 @@ namespace Cropper.AnimatedGif
         {
             InitializeComponent();
 
-            _settings = settings;
+            _settings = settings;  // cache
 
-            this.chkPopViewer.Checked = _settings.PopViewer;
             this.chkPlaySound.Checked = _settings.PlaySound;
             this.cmbInterval.SelectedItem = _settings.CaptureInterval.ToString();
             this.cmbQuality.SelectedItem = _settings.EncodingQuality.ToString();
@@ -24,11 +23,43 @@ namespace Cropper.AnimatedGif
                 this.cmbRepeats.SelectedItem = "none";
             else
                 this.cmbRepeats.Text = _settings.Repeats.ToString();
+
+
+            if (_settings.ViewCapture.ToLower() == "none")
+                this.radioViewNone.Checked = true;
+            else if (_settings.ViewCapture.ToLower() == "default")
+                this.radioViewDefault.Checked = true;
+            else if (_settings.ViewCapture.ToLower() == "specified")
+            {
+                this.radioViewSpecified.Checked = true;
+            }
+            else
+            {
+                // invalid value in the settings.
+                this.radioViewDefault.Checked = true;
+                _settings.ViewCapture = "default";
+            }
+            this.txtViewerProgram.Text = _settings.ViewerProgram;
+
+            radioViewer_CheckedChanged(null,null);
+        }
+
+
+        private string GetViewCapture()
+        {
+            if (this.radioViewNone.Checked)
+                return "None";
+            if (this.radioViewDefault.Checked)
+                return "Default";
+            if (this.radioViewSpecified.Checked)
+                return "Specified";
+            return "Default";
         }
 
         public void ApplySettings()
         {
-            _settings.PopViewer = this.chkPopViewer.Checked;
+            _settings.ViewCapture = GetViewCapture();
+            _settings.ViewerProgram = this.txtViewerProgram.Text;
             _settings.PlaySound = this.chkPlaySound.Checked;
             _settings.CaptureInterval = Int32.Parse(this.cmbInterval.Text);
             _settings.EncodingQuality = Int32.Parse(this.cmbQuality.Text);
@@ -54,12 +85,51 @@ namespace Cropper.AnimatedGif
             }
         }
 
+
+
+        private void radioViewer_CheckedChanged(Object sender, EventArgs e)
+        {
+            if (this.radioViewSpecified.Checked)
+            {
+                this.txtViewerProgram.Enabled = true;
+                this.btnBrowse.Enabled = true;
+            }
+            else
+            {
+                this.txtViewerProgram.Enabled = false;
+                this.btnBrowse.Enabled = false;
+            }
+        }
+
+
+        private void btnBrowse_Click(Object sender, EventArgs e)
+        {
+            using (var dlg = new System.Windows.Forms.OpenFileDialog())
+            {
+                dlg.Filter = "EXE (*.exe)|*.exe";
+                if (!String.IsNullOrEmpty(this.txtViewerProgram.Text))
+                {
+                    dlg.InitialDirectory = System.IO.Path.GetDirectoryName(this.txtViewerProgram.Text);
+                }
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    this.txtViewerProgram.Text = dlg.FileName;
+                }
+            }
+        }
+
+
+
         private void InitializeComponent()
         {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.radioViewNone = new System.Windows.Forms.RadioButton();
+            this.radioViewDefault = new System.Windows.Forms.RadioButton();
+            this.radioViewSpecified = new System.Windows.Forms.RadioButton();
+            this.txtViewerProgram = new System.Windows.Forms.TextBox();
             this.chkPlaySound = new System.Windows.Forms.CheckBox();
             this.lblPlaySound = new System.Windows.Forms.Label();
-            this.chkPopViewer = new System.Windows.Forms.CheckBox();
-            this.lblPopViewer = new System.Windows.Forms.Label();
+            this.lblViewer = new System.Windows.Forms.Label();
             this.cmbInterval= new System.Windows.Forms.ComboBox();
             this.lblInterval = new System.Windows.Forms.Label();
             this.cmbQuality= new System.Windows.Forms.ComboBox();
@@ -92,8 +162,12 @@ namespace Cropper.AnimatedGif
             this.tabPage1.Controls.Add(this.cmbInterval);
             this.tabPage1.Controls.Add(this.lblPlaySound);
             this.tabPage1.Controls.Add(this.chkPlaySound);
-            this.tabPage1.Controls.Add(this.lblPopViewer);
-            this.tabPage1.Controls.Add(this.chkPopViewer);
+            this.tabPage1.Controls.Add(this.lblViewer);
+            this.tabPage1.Controls.Add(this.txtViewerProgram);
+            this.tabPage1.Controls.Add(this.radioViewNone);
+            this.tabPage1.Controls.Add(this.radioViewDefault);
+            this.tabPage1.Controls.Add(this.radioViewSpecified);
+            this.tabPage1.Controls.Add(this.btnBrowse);
             //
             // chkPlaySound
             //
@@ -184,22 +258,71 @@ namespace Cropper.AnimatedGif
             this.lblRepeats.TabIndex = 40;
             this.lblRepeats.Text = "Playback Repeats:";
             //
-            // chkPopViewer
+            // lblViewer
             //
-            this.chkPopViewer.Location = new System.Drawing.Point(104, 118);
-            this.chkPopViewer.Text = "";
-            this.chkPopViewer.Name = "chkPopViewer";
-            this.chkPopViewer.TabIndex = 11;
-            this.tooltip.SetToolTip(chkPopViewer, "check to open the image in\nthe default viewer after capture.");
+            this.lblViewer.AutoSize = true;
+            this.lblViewer.Location = new System.Drawing.Point(4, 122);
+            this.lblViewer.Name = "lblViewer";
+            this.lblViewer.Size = new System.Drawing.Size(68, 13);
+            this.lblViewer.TabIndex = 60;
+            this.lblViewer.Text = "Viewer?";
             //
-            // lblPopViewer
+            // radioViewNone
             //
-            this.lblPopViewer.AutoSize = true;
-            this.lblPopViewer.Location = new System.Drawing.Point(4, 122);
-            this.lblPopViewer.Name = "lblPopViewer";
-            this.lblPopViewer.Size = new System.Drawing.Size(68, 13);
-            this.lblPopViewer.TabIndex = 60;
-            this.lblPopViewer.Text = "Pop Viewer?";
+            this.radioViewNone.AutoSize = true;
+            this.radioViewNone.Location = new System.Drawing.Point(104, 118);
+            this.radioViewNone.Name = "radioViewNone";
+            this.radioViewNone.Size = new System.Drawing.Size(64, 17);
+            this.radioViewNone.TabIndex = 51;
+            this.radioViewNone.Text = "None";
+            this.radioViewNone.UseVisualStyleBackColor = true;
+            this.radioViewNone.CheckedChanged += radioViewer_CheckedChanged;
+            this.tooltip.SetToolTip(radioViewNone, "Don't view the animated gif\nafter capture.");
+            //
+            // radioViewDefault
+            //
+            this.radioViewDefault.AutoSize = true;
+            this.radioViewDefault.Location = new System.Drawing.Point(104, 142);
+            this.radioViewDefault.Name = "radioViewDefault";
+            this.radioViewDefault.Size = new System.Drawing.Size(64, 17);
+            this.radioViewDefault.TabIndex = 61;
+            this.radioViewDefault.Text = "Default";
+            this.radioViewDefault.UseVisualStyleBackColor = true;
+            this.radioViewDefault.CheckedChanged += radioViewer_CheckedChanged;
+            this.tooltip.SetToolTip(radioViewDefault, "view the animated gif in the default viewer\nafter capture.");
+            //
+            // radioViewSpecified
+            //
+            this.radioViewSpecified.AutoSize = true;
+            this.radioViewSpecified.Location = new System.Drawing.Point(104, 166);
+            this.radioViewSpecified.Name = "radioViewSpecified";
+            this.radioViewSpecified.Size = new System.Drawing.Size(64, 17);
+            this.radioViewSpecified.TabIndex = 71;
+            this.radioViewSpecified.Text = "Specified";
+            this.radioViewSpecified.UseVisualStyleBackColor = true;
+            this.radioViewSpecified.CheckedChanged += radioViewer_CheckedChanged;
+            this.tooltip.SetToolTip(radioViewSpecified, "view the animated gif in the specified\nviewer after capture.");
+            //
+            // btnBrowse
+            //
+            this.btnBrowse.Location = new System.Drawing.Point(186, 166);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(32, 20);
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.TabIndex = 81;
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Enabled = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            this.tooltip.SetToolTip(btnBrowse, "Find Program...");
+            //
+            // txtViewerProgram
+            //
+            this.txtViewerProgram.Location = new System.Drawing.Point(4, 192);
+            this.txtViewerProgram.Name = "txtViewerProgram";
+            this.txtViewerProgram.Size = new System.Drawing.Size(310, 20);
+            this.txtViewerProgram.TabIndex = 91;
+            this.txtViewerProgram.Enabled = false;
+            this.tooltip.SetToolTip(txtViewerProgram, "The program to use to edit or view\nthe animated gif.");
             //
             // Options
             //
@@ -216,8 +339,6 @@ namespace Cropper.AnimatedGif
 
         private System.Windows.Forms.CheckBox chkPlaySound;
         private System.Windows.Forms.Label lblPlaySound;
-        private System.Windows.Forms.CheckBox chkPopViewer;
-        private System.Windows.Forms.Label lblPopViewer;
         private System.Windows.Forms.ComboBox cmbInterval;
         private System.Windows.Forms.Label lblInterval;
         private System.Windows.Forms.ComboBox cmbQuality;
@@ -225,6 +346,12 @@ namespace Cropper.AnimatedGif
         private System.Windows.Forms.ComboBox cmbRepeats;
         private System.Windows.Forms.Label lblRepeats;
         private System.Windows.Forms.ToolTip tooltip;
+        private System.Windows.Forms.Label lblViewer;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.RadioButton radioViewNone;
+        private System.Windows.Forms.RadioButton radioViewDefault;
+        private System.Windows.Forms.RadioButton radioViewSpecified;
+        private System.Windows.Forms.TextBox txtViewerProgram;
     }
 }
 
